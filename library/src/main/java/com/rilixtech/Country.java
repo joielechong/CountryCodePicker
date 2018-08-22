c74947c746cddcf2b8d9f24169a39a0e5d7faf4c
joielechong <juansspy@gmail.com>
2018-08-22 14:35:08 +0700

    Make final member of Country

--- library/src/main/java/com/rilixtech/Country.java
+++ library/src/main/java/com/rilixtech/Country.java
@@ -7,11 +7,14 @@ package com.rilixtech;
  * a pojo should be a pojo and no more.
  * Updated by Joielechong 13 May 2017
  *
+ * Make class member final and remove setter.
+ * Updated by Joielechong 22 August 2018
+ *
  */
 public class Country {
-  private String iso;
-  private String phoneCode;
-  private String name;
+  private final String iso;
+  private final String phoneCode;
+  private final String name;
 
   public Country(String iso, String phoneCode, String name) {
     this.iso = iso;
@@ -23,24 +26,12 @@ public class Country {
     return iso;
   }
 
-  public void setIso(String iso) {
-    this.iso = iso;
-  }
-
   public String getPhoneCode() {
     return phoneCode;
   }
 
-  public void setPhoneCode(String phoneCode) {
-    this.phoneCode = phoneCode;
-  }
-
   public String getName() {
     return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
   }
 
   /**
